8959bdc574ca519330bb00c76b944c0f107df5f0
RichardGill-apl <richard.gill@footballaustralia.com.au>
2021-07-29 10:40:21 +1000

    Featured Article Styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-import RelatedNews from '../components/RelatedNews';
+import FeaturedArticle from '../components/FeaturedArticle'
+import RelatedNews from '../components/RelatedNews'
 
 export default function Home() {
   return (
-      <RelatedNews />
+      <>
+        <FeaturedArticle />
+        <RelatedNews />
+      </>
   )
 }
